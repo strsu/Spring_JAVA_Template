c1318adbc66a210d82c0a5fe490c6d3aa76a201d
yj.park <jolie0215@gmail.com>
2024-08-15 09:25:56 +0900

    기본 인증처리 코드

---
+++ src/main/java/com/example/template/global/config/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.template.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+                // Auth가 없을 때 response status를 결정함
+                .exceptionHandling()
+                // Auth가 없다고 이후 로직이 실행이 안되는 것은 아니다.
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다.
+                .requestMatchers("/api/member/**").permitAll() // 회원가입 api
+                .anyRequest().authenticated(); // 그 외 인증 없이 접근X
+
+
+        return httpSecurity.build();
+    }
+}
+
